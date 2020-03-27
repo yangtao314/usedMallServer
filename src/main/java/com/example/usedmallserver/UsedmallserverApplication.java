fb86687cda3dfcb9f8e26f4fe826be5c091a238f
5201314@fmj <a1316422632@163.com>
2020-03-27 22:34:32 +0800

    init server

---
+++ src/main/java/com/example/usedmallserver/UsedmallserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.usedmallserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedmallserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsedmallserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/usedmallserver/UsedmallserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usedmallserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedmallserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
